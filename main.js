e6ccbabf43e62062e749e0e86f52db7050c54672
Umpoy <iumpoy@yahoo.com>
2018-01-08 14:16:39 -0800

    buttons light up and make sounds when clicked

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
+var green_audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3')
+var red_audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3')
+var yellow_audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3')
+var blue_audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
+var game_over_audio = new Audio('http://www.freesound.org/data/previews/331/331912_3248244-lq.mp3')
+
+
 $(document).ready(function () {
     assignClickHandlers()
 })
 
 function assignClickHandlers() {
-    $('.button').click(light_up_element)
+    $('.button').click(light_up_element);
+    $('#green').on('click', function () {
+        green_audio.play('')
+    });
+    $('#red').on('click', function () {
+        red_audio.play('')
+    });
+    $('#yellow').on('click', function () {
+        yellow_audio.play('')
+    });
+    $('#blue').on('click', function () {
+        blue_audio.play('')
+    });
+
 }
 
 function light_up_element() {
